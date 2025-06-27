9746b117448557b21a1b465a32663616a4482687
vishnupraneeth450 <vishnupraneeth450@gmail.com>
2025-06-27 18:32:46 +0530

    created func for linked list

--- fuck.py
+++ fuck.py
@@ -1,3 +1,15 @@
-def display(a,b):
-    print(a+b)
-    return
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+node=Node(0)
+class LinkedList:
+    def display(self,a,b):
+        print(a+b)
+        return
+    def create(self,number):
+        node1=Node(number)
+        node.next=node1
+        node=node1
+        return 
+    
